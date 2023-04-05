9bc24dd5a931ffe3dc6ee7c93ba4fd926bcc14a9
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2023-04-06 01:46:36 +0530

    added drawer

--- App.tsx
+++ App.tsx
@@ -1,44 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Platform } from 'react-native';
-
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Button, lightColors, createTheme, ThemeProvider } from '@rneui/themed';
 import * as React from 'react';
-
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-const theme = createTheme({
-  lightColors: {
-    ...Platform.select({
-      default: lightColors.platform.android,
-      ios: lightColors.platform.ios,
-    }),
-  },
-});
 
-
-function HomeScreen() {
+function HomeScreen({ navigation }:{navigation:any}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text> Screen</Text>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
     </View>
   );
 }
-const Stack = createNativeStackNavigator();
-const App = () => {
 
+function NotificationsScreen({ navigation }:{navigation:any}) {
   return (
-    <SafeAreaProvider>
-
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
-  )
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
 }
 
-export default App
+const Drawer = createDrawerNavigator();
 
-const styles = StyleSheet.create({})
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
