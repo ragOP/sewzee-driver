eb9766138422a43d196f5c97fd05fdc59f84ad0d
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2023-04-06 16:57:34 +0530

    Added Slider

--- App.tsx
+++ App.tsx
@@ -1,20 +1,54 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View,Text,TouchableOpacity,StyleSheet  } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
-function HomeScreen({ navigation }:{navigation:any}) {
+import CardSilder from 'react-native-cards-slider';
+
+function HomeScreen({ navigation }: { navigation: any }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
+      {/* <AdvancedCardCarousel/> */}
+      
+        {/* <Text style={{fontSize: 30, color: '#000', marginTop:40, marginLeft:20, fontWeight:'bold'}}>
+          raghib
+        </Text> */}
+        <CardSilder style={{marginTop: 30}}>
+          <View style={[styles.card, styles.elevation]}>
+            <Text style={{color: 'white', fontSize: 24, fontWeight: 'bold'}}>
+              sA
+            </Text>
+          </View>
+          <View style={[styles.card, styles.elevation]}>
+            <Text style={{color: 'white', fontSize: 24, fontWeight: 'bold'}}>
+              A2
+            </Text>
+          </View>
+          <View style={{height: 170, justifyContent:'center', alignItems:'center', backgroundColor: 'white'}}>
+            <Text style={{color: 'black', fontSize: 24, fontWeight: 'bold'}}>
+              B
+            </Text>
+          </View>
+          <View style={{height: 170, justifyContent:'center', alignItems:'center', backgroundColor: 'teal'}}>
+            <Text style={{color: 'white', fontSize: 24, fontWeight: 'bold'}}>
+             C
+            </Text>
+          </View>
+          <TouchableOpacity  
+           onPress={() => navigation.navigate('Notifications')}
+          style={{height: 170, justifyContent:'center', alignItems:'center', backgroundColor: 'lightpink'}}>
+            <Text style={{color: 'white', fontSize: 24, fontWeight: 'bold'}}>
+              D
+            </Text>
+          </TouchableOpacity >
+        </CardSilder>
+      
+
     </View>
   );
 }
 
-function NotificationsScreen({ navigation }:{navigation:any}) {
+function NotificationsScreen({ navigation }: { navigation: any }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button onPress={() => navigation.goBack()} title="Go back home" />
@@ -27,10 +61,42 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator 
+     screenOptions={{
+      drawerStyle: {
+        backgroundColor: 'white',
+        width: 240,
+      },
+    }}
+      
+      
+      
+      initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Notifications" component={NotificationsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 13,
+  },
+  card: {
+    height: 170,
+    justifyContent:'center',
+     alignItems:'center',
+    backgroundColor: 'teal',
+    borderRadius: 8,
+    paddingVertical: 45,
+    paddingHorizontal: 25,
+    width: '100%',
+    marginVertical: 10,
+  },
+  elevation: {
+    elevation: 20,
+    shadowColor: '#52006A',
+  },
+});
---
+++ components/SlidingCard.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SlidingCard = () => {
+  return (
+    <View style={{height: 170, justifyContent:'center', alignItems:'center', backgroundColor: 'skyblue'}}>
+            <Text style={{color: 'white', fontSize: 24, fontWeight: 'bold'}}>
+              A
+            </Text>
+          </View>
+  )
+}
+
+export default SlidingCard
+
+const styles = StyleSheet.create({})
